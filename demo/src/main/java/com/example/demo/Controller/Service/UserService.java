ccd53c2559ca610b281cc9595fb273c62cf7c0f0
rohi2208 <rohitsdevadkar@gmail.com>
2020-01-02 07:44:06 +0000

    Spring Boot Details

---
+++ demo/src/main/java/com/example/demo/Controller/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.demo.Controller.Service;
+
+import com.example.demo.users.shared.UserDTO;
+
+public interface UserService {
+   UserDTO createuserdetails(UserDTO userdetails);
+}
---
+++ demo/src/main/java/com/example/demo/Controller/Service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller.Service;
+
+import com.example.demo.users.shared.UserDTO;
+
+class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDTO createuserdetails(UserDTO userdetails) {
+        return null;
+    }
+    
+}
